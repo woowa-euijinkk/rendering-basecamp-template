52b59e460dd9d10219cf32bfd1052750873d7208
김의진 <gytks4@naver.com>
2025-09-01 15:39:54 +0900

    timeout 제거

--- react-hybrid/src/client/lib/apiClient.ts
+++ react-hybrid/src/client/lib/apiClient.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const apiClient = axios.create({
   baseURL: "https://api.themoviedb.org/3",
-  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
